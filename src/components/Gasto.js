b120adc780d112bf6e84640ef7da26fbb4ea9d8d
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-10 10:53:18 -0600

    Mostrando imagenes de la categoria de gasto

--- src/components/Gasto.js
+++ src/components/Gasto.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import globalStyles from '../styles';
+import { formatearCantidad } from '../helpers';
+
+const diccionarioIconos =
+{
+    ahorro: require('../img/icono_ahorro.png'),
+    comida: require('../img/icono_comida.png'),
+    casa: require('../img/icono_casa.png'),
+    gastos: require('../img/icono_gastos.png'),
+    ocio: require('../img/icono_ocio.png'),
+    salud: require('../img/icono_salud.png'),
+    suscripciones: require('../img/icono_suscripciones.png'),
+}
 
 const Gasto = 
 (
@@ -13,7 +25,20 @@ const Gasto =
 
     return (
         <View style={styles.contenedor}>
-            <Text>{nombre}</Text>
+            <View>
+                <View>
+                    <Image 
+                        source={diccionarioIconos[categoria]}
+                    />
+
+                    <View>
+                        <Text>{categoria}</Text>
+                        <Text>{nombre}</Text>
+                    </View>
+                </View>
+
+                <Text>{formatearCantidad(cantidad)}</Text>                
+            </View>
         </View>
     )
 }
